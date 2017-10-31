e3710660ef252db229a50791d9c1043e70bf52e9
ethien-salinas <ethien.salinas@gmail.com>
2017-10-30 20:08:17 -0600

    first commit

---
+++ src/main/java/com/czes/samp/SAMP/Backend/SampBackendApplication.java
@@ -0,0 +1,12 @@
+package com.czes.samp.SAMP.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampBackendApplication.class, args);
+	}
+}
