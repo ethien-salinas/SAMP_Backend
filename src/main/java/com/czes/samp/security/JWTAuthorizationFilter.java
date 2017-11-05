6547b77073943212eafeb5173a0e8c7b38504765
Ethien Salinas <ethien.salinas@gmail.com>
2017-11-05 00:50:34 -0600

    [ADD] JWT support

---
+++ src/main/java/com/czes/samp/security/JWTAuthorizationFilter.java
@@ -0,0 +1,65 @@
+package com.czes.samp.security;
+
+import io.jsonwebtoken.Jwts;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+	@Value("${application.jwt.secret}")
+	private String jwtSecret;
+	@Value("${application.jwt.header-string}")
+	private String jwtHeaderString;
+	@Value("${application.jwt.token-prefix}")
+	private String jwtTokenPrefix;
+
+    public JWTAuthorizationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws IOException, ServletException {
+        String header = req.getHeader(jwtHeaderString);
+
+        if (header == null || !header.startsWith(jwtTokenPrefix)) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(jwtHeaderString);
+        if (token != null) {
+            // parse the token.
+            String user = Jwts.parser()
+                    .setSigningKey(jwtSecret.getBytes())
+                    .parseClaimsJws(token.replace(jwtTokenPrefix, ""))
+                    .getBody()
+                    .getSubject();
+
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+}
