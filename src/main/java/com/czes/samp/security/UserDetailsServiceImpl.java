8825d49faf11e7c5f26b8658bc84592a44889421
Ethien Salinas <ethien.salinas@gmail.com>
2017-11-09 07:00:56 -0600

    [ADD] custom implementation of UserDetailsService to in-memory

---
+++ src/main/java/com/czes/samp/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.czes.samp.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.czes.samp.entity.ApplicationUser;
+import com.czes.samp.repository.ApplicationUserRepository;
+
+import static java.util.Collections.emptyList;
+
+/*
+* UserDetailsService implementation to be used with the in-memory database
+*/
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private ApplicationUserRepository applicationUserRepository;
+
+    public UserDetailsServiceImpl(ApplicationUserRepository applicationUserRepository) {
+        this.applicationUserRepository = applicationUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+        if (applicationUser == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
+    }
+}
