a0211a9dc10314f92a89b50c41cfdc0b06005c5c
Ethien Salinas <ethien.salinas@gmail.com>
2017-11-10 07:42:36 -0600

    [UPDATE] format

--- src/main/java/com/czes/samp/controller/UserController.java
+++ src/main/java/com/czes/samp/controller/UserController.java
@@ -11,21 +11,21 @@ import com.czes.samp.repository.ApplicationUserRepository;
 
 @RestController
 @RequestMapping("/user")
-public class UserController{
+public class UserController {
 
 	private ApplicationUserRepository applicationUserRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserController(ApplicationUserRepository applicationUserRepository,
-                          BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.applicationUserRepository = applicationUserRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+	public UserController(ApplicationUserRepository applicationUserRepository,
+			BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.applicationUserRepository = applicationUserRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
-    @PostMapping("/sign-up")
-    public void signUp(@RequestBody ApplicationUser user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        applicationUserRepository.save(user);
-    }
+	@PostMapping("/sign-up")
+	public void signUp(@RequestBody ApplicationUser user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		applicationUserRepository.save(user);
+	}
 
 }
